489118b14313b745ba2059210508cdc5333f2cc8
twinkle <twinklekumari246@gmail.com>
2025-07-11 01:14:56 +0530

    second version of hotel

--- db.js
+++ db.js
@@ -1,16 +1,18 @@
 //very important
 
 const mongoose = require('mongoose');
+require('dotenv').config();
 //define the mongoDB connection url
-const mongoURL = 'mongodb://localhost:27017/hotels'
+// const mongoURL = process.env.DB_URL_LOCAL'
 
+const mongoURL = process.env.DB_URL;
 
 //set up connection(ye required parameter hai)
 mongoose.connect(mongoURL,{
     useNewURlParser:true,
     useUnifiedTopology: true
 })
-
+// IDoz8a6vbDquahX1 cQaeJ5WWswVFfFpq
 //isi connection se hum apne db server and nodejs server ko connect krte hsi 
 //isse mongoose khud maintain karta hai
 const db = mongoose.connection;
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 
 app.get('/',(req,res) => {
@@ -44,6 +46,6 @@ app.use('/menu',menuRoutes)
 const personRoutes = require('./router/personRoutes')
 app.use('/person',personRoutes);
 
-app.listen(3000,() => {
+app.listen(PORT,() => {
     console.log("server is live");
 })
